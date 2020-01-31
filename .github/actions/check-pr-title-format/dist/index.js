b90ac376c514b052e067067269793982514545f0
VignesH KumaR <kvignsh@gmail.com>
2020-01-31 12:25:14 +0530

    Updated pr-title-format checker action with label-adder

--- .github/actions/check-pr-title-format/dist/index.js
+++ .github/actions/check-pr-title-format/dist/index.js
@@ -3881,7 +3881,9 @@ isStream.transform = function (stream) {
 const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
-function run() {
+async function run() {
+
+  const repoToken = core.getInput('repo-token');
   const pattern = core.getInput('pattern');
   const regex = new RegExp(pattern);
   const title =
@@ -3889,15 +3891,52 @@ function run() {
     github.context.payload.pull_request &&
     github.context.payload.pull_request.title;
   
+  const {
+    payload: { pull_request: pullRequest, repository }
+  } = github.context;
+  const { number: issueNumber } = pullRequest;
+  const { full_name: repoFullName } = repository;
+  const [owner, repo] = repoFullName.split("/");
+
+  const octokit = new github.GitHub(repoToken);
+
   core.info(title);
   const isValid = regex.test(title)
   if (!isValid) {
     core.setOutput("pr-title-format", "invalid");
     core.setFailed(
       `Pull request title "${title}" does not match regex pattern "${pattern}".`,
-    )
+    );
+
+    let response = await octokit.issues.createLabel({ 
+      owner, 
+      repo, 
+      name: 'check-pr-title-format', 
+      color: '#ff0000', 
+      description: 'Label to identify whether the PR title is in the required format' 
+    });
+
+    console.log('Label created !!!!');
+    console.log(response)
+    console.log(`Added the new label to the issue #{issueNumber}`);
+    if(response) {
+      await octokit.issues.addLabels({ 
+        owner, 
+        repo, 
+        issueNumber, 
+        labels: ['check-pr-title-format']
+      });
+    }
+
   } else {
     core.setOutput("pr-title-format", "valid");
+    console.log(`Removing the 'check-pr-title-format' label from the issue #{issueNumber}`);
+    await octokit.issues.removeLabel({ 
+      owner, 
+      repo, 
+      issueNumber, 
+      name: 'check-pr-title-format'
+    });
   }
 }
 
--- .github/actions/check-pr-title-format/main.js
+++ .github/actions/check-pr-title-format/main.js
@@ -1,7 +1,9 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-function run() {
+async function run() {
+
+  const repoToken = core.getInput('repo-token');
   const pattern = core.getInput('pattern');
   const regex = new RegExp(pattern);
   const title =
@@ -9,15 +11,52 @@ function run() {
     github.context.payload.pull_request &&
     github.context.payload.pull_request.title;
   
+  const {
+    payload: { pull_request: pullRequest, repository }
+  } = github.context;
+  const { number: issueNumber } = pullRequest;
+  const { full_name: repoFullName } = repository;
+  const [owner, repo] = repoFullName.split("/");
+
+  const octokit = new github.GitHub(repoToken);
+
   core.info(title);
   const isValid = regex.test(title)
   if (!isValid) {
     core.setOutput("pr-title-format", "invalid");
     core.setFailed(
       `Pull request title "${title}" does not match regex pattern "${pattern}".`,
-    )
+    );
+
+    let response = await octokit.issues.createLabel({ 
+      owner, 
+      repo, 
+      name: 'check-pr-title-format', 
+      color: '#ff0000', 
+      description: 'Label to identify whether the PR title is in the required format' 
+    });
+
+    console.log('Label created !!!!');
+    console.log(response)
+    console.log(`Added the new label to the issue #{issueNumber}`);
+    if(response) {
+      await octokit.issues.addLabels({ 
+        owner, 
+        repo, 
+        issueNumber, 
+        labels: ['check-pr-title-format']
+      });
+    }
+
   } else {
     core.setOutput("pr-title-format", "valid");
+    console.log(`Removing the 'check-pr-title-format' label from the issue #{issueNumber}`);
+    await octokit.issues.removeLabel({ 
+      owner, 
+      repo, 
+      issueNumber, 
+      name: 'check-pr-title-format'
+    });
   }
 }
 
